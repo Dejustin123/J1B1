53670959cbe73abde67a0c58033ebf09f5e6a121
Justin <jusvz2000@gmail.com>
2018-09-04 11:11:48 +0200

    opdracht 1 javascript

---
+++ J1B1W2. javascript/java.js
@@ -0,0 +1,4 @@
+alert("wij gaan u 3 vragen stellen");
+document.write ('uw voornaam is:'+ prompt("wat is uw voornaam")+ "<br>");
+document.write ('uw achternaam is:'+ prompt( "wat is uw achternaam")+ "<br>");
+document.write ('uw leeftijd is:'+ prompt( "wat is uw leeftijd")+ "<br>");
