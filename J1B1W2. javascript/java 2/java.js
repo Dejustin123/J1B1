70752d035a104164eeb07e89b0985063cd996278
Justin <jusvz2000@gmail.com>
2018-09-04 13:15:16 +0200

    nieuwe var

---
+++ J1B1W2. javascript/java 2/java.js
@@ -0,0 +1,8 @@
+const vraag="wij gaan u 3 vragen stellen";
+alert(vraag);
+var voornaam = prompt("wat is uw voornaam");
+var achternaam = prompt("wat is uw achternaam");
+var leeftijd= prompt("wat is uw leeftijd");
+document.write("voornaam : " + voornaam + "<br>");
+document.write("achternaam : " + achternaam + "<br>");
+document.write("leeftijd : " + leeftijd + "<br>");
