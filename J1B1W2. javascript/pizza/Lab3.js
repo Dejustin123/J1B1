cd581542f58d46908c94ceb5b6e0da14479075e6
Justin <jusvz2000@gmail.com>
2018-09-05 11:52:45 +0200

    opdracht 3 en pizza

---
+++ J1B1W2. javascript/pizza/Lab3.js
@@ -0,0 +1,6 @@
+var Small = prompt("hoeveel pizza's wilt u");
+var Medium = prompt("hoeveel pizza's wilt u");
+var Large = prompt("hoeveel pizza's wilt u");
+document.write( 'small pizza prijs: € ' +6.10 * Small + "<br>");
+document.write( 'medium pizza prijs: € ' +8.99 * Medium + "<br>");
+document.write( 'Large pizza prijs: € ' + 12.10 * Large + "<br>");
