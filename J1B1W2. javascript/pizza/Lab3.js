f46686040c6c036c945fe352e00fec16fba961b0
Justin <jusvz2000@gmail.com>
2018-09-12 11:20:29 +0200

    new changes pizza

--- J1B1W2. javascript/pizza/Lab3.js
+++ J1B1W2. javascript/pizza/Lab3.js
@@ -3,13 +3,17 @@ var Medium = prompt("hoeveel medium pizza's wilt u");
 var Large = prompt("hoeveel large pizza's wilt u");
 //alle variabelen voor de pizza's//*
 
-const totalsmall=(Small*6.10 );
-const totalmedium=(Medium*8.99 );
-const totallarge=(Large*12.10 );
+const priceSmall = 5.50;
+const priceMedium = 8.99;
+const priceLarge = 12.10;
+
+const totalsmall=(Small*priceSmall );
+const totalmedium=(Medium*priceMedium );
+const totallarge=(Large*priceLarge );
 //Constanten voor alle pizza's//*
 
-document.write( 'small pizza prijs: € ' +6.10 * Small + "<br>");// totaal prijs small pizza's//*
-document.write( 'medium pizza prijs: € ' +8.99 * Medium + "<br>");//totaal prijs medium pizza's//*
-document.write( 'Large pizza prijs: € ' + 12.10 * Large + "<br>");//totaal prijs large pizza's//*
+document.write( 'small pizza prijs: € ' + totalsmall + "<br>");// totaal prijs small pizza's//*
+document.write( 'medium pizza prijs: € ' +totalmedium + "<br>");//totaal prijs medium pizza's//*
+document.write( 'Large pizza prijs: € ' + totallarge + "<br>");//totaal prijs large pizza's//*
 var totaal = totalsmall+totalmedium+totallarge;//variable totaal prijs//*
 document.write("totaal €:"+totaal );//totaal prijs alle pizza's bij //*
